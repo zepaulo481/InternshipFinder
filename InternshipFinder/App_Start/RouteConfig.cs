281584d177a0cbb008110d1fa17537d48d8dfd4e
José Machado <jose.machado.734@gmail.com>
2017-12-17 21:51:58 +0000

    Primeira grande alteração
    Gestão do layout e index e outras pequenas alterações

--- InternshipFinder/App_Start/RouteConfig.cs
+++ InternshipFinder/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace InternshipFinder
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Estagios", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ InternshipFinder/Controllers/EstagiosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternshipFinder.Controllers
+{
+    public class EstagiosController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Procurar()
+        {
+            return View();
+        }
+
+        public ActionResult Inserir()
+        {
+            return View();
+        }
+    }
+}
